f80a682b64054fe79a35f737a46fa5a9e43e5e70
Bekzat <bekapng@gmail.com>
2025-08-23 12:38:33 +0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
     return (
         <div className="text-black">
             BEKA LOX
+            AYAN GAY
 
 
             <p className='text-5xl'>CELTIC ONE LOVE</p>
