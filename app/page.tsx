13b1474073e3e8d6618344bdb46be7abd939a1b3
Ayanqq <nazbievayan16@gmail.com>
2025-09-06 11:28:46 +0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,30 +13,30 @@ import ContactSection from './components/ContactSection';
 export default function Home() {
     const [currentSection, setCurrentSection] = useState('hero');
 
-    useEffect(() => {
-        function fixScale() {
-            // допустим "ноутбуки" — это ширина >= 1024px
-            if (window.innerWidth >= 1024) {
-                const scaleFix = 1 / window.devicePixelRatio;
-
-                document.body.style.transform = `scale(${scaleFix})`;
-                document.body.style.transformOrigin = '0 0';
-
-                document.body.style.width = 100 * window.devicePixelRatio + '%';
-                document.body.style.height = 100 * window.devicePixelRatio + '%';
-            } else {
-                // сбросить изменения, если снова зашли на мобилке
-                document.body.style.transform = '';
-                document.body.style.width = '';
-                document.body.style.height = '';
-            }
-        }
-
-        fixScale();
-        window.addEventListener('resize', fixScale);
-
-        return () => window.removeEventListener('resize', fixScale);
-    }, []);
+    // useEffect(() => {
+    //     function fixScale() {
+    //         // допустим "ноутбуки" — это ширина >= 1024px
+    //         if (window.innerWidth >= 1024) {
+    //             const scaleFix = 1 / window.devicePixelRatio;
+    //
+    //             document.body.style.transform = `scale(${scaleFix})`;
+    //             document.body.style.transformOrigin = '0 0';
+    //
+    //             document.body.style.width = 100 * window.devicePixelRatio + '%';
+    //             document.body.style.height = 100 * window.devicePixelRatio + '%';
+    //         } else {
+    //             // сбросить изменения, если снова зашли на мобилке
+    //             document.body.style.transform = '';
+    //             document.body.style.width = '';
+    //             document.body.style.height = '';
+    //         }
+    //     }
+    //
+    //     fixScale();
+    //     window.addEventListener('resize', fixScale);
+    //
+    //     return () => window.removeEventListener('resize', fixScale);
+    // }, []);
 
     const sections = [
         'hero',
